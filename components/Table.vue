05bf96dedf436bd13bd9b0f648c16b00b3c8fe5b
Sergey <zavulon31990@gmail.com>
2021-03-01 21:57:10 +0300

    first commit

---
+++ components/Table.vue
@@ -0,0 +1,29 @@
+<template>
+    <table>
+    <tr>
+        <td v-for="(pers, index) in person" :key="index">{{pers}}</td>
+        
+    </tr>
+</table>
+</template>
+
+
+<script>
+export default {
+    props: ['person']
+}
+
+</script>
+
+
+
+<style>
+table{
+    width: 100%;
+}
+table tr td {
+    border: 1px solid black;
+    height: 50px;
+    text-align: center;
+}
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+  <Table :person="person"/>
+</div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+
+<script>
+import Table from '../components/Table'
 export default {
-  name: 'App',
+  name: 'app',
+  data(){
+    return {
+      person: [
+        {
+          name: 'Sergey',
+          age: 30
+        },
+        {
+          name: 'Tatyana',
+          age: 34
+        }
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    Table
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
